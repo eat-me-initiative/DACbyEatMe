0d45310dd0ecda2dfa7094c00c2d12c254d163d9
Yoko Arigato <danmee96@gmail.com>
2019-02-17 01:30:46 -0500

    i was a retard. i had some needless code that conflicted with preexisting code

--- DACbyEatMe/Form1.cs
+++ DACbyEatMe/Form1.cs
@@ -28,14 +28,7 @@ namespace DACbyEatMe
     {  //place holder
        //public string[] proxies = new WebClient().DownloadString("https://proxyscrape.com/api?request=getproxies&proxytype=http&timeout=10000&country=all&ssl=all&anonymity=all").Split('\n');
         public string fileName = string.Format("tokens/{0:yyyy-MM-dd_HH-mm-ss}.txt", (object)DateTime.Now);
-        private IContainer components = (IContainer)null;
-        private Button start;
-        private Button getProxies;
-        private TextBox username;
-        private TextBox inviteBox;
-        private TextBox textBox3;
-        private CheckBox randomChecked;
-        private CheckBox checkBox2;
+        
         
 
 
